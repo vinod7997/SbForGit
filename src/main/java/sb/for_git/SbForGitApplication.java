1ed06020de2cf84b6dd056745b61c69c3c8ad126
vinod7997 <vinodreddyupparapalli@gmail.com>
2025-04-09 18:00:25 +0530

    Initial Commit

---
+++ src/main/java/sb/for_git/SbForGitApplication.java
@@ -0,0 +1,13 @@
+package sb.for_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbForGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sb/for_git/SbForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.for_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbForGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
